2c8a19945f350d976386d44e01f7c5aa699fbb7f
aluno <aluno@LAB.ETEC187>
2022-09-05 20:43:16 -0300

    Criando a tela Home

---
+++ Meus-Filmes/src/pages/Home.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+import { View, Text, TextInput, TouchableOpacity, StyleSheet} from "react-native" ;
+  export default function Home(){
+      return(
+          <View>
+              <Text> Meus filmes</Text>
+              <Text> Ola Kauan, Boa NOite </Text>
+
+      );
+  }
